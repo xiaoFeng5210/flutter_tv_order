d1b18de97bbce6789390e2a4e6cc527342c59e23
Eadweard <aurora.coder.zqf@gmail.com>
2024-06-13 15:42:28 +0800

    测试自动播放语音

--- lib/widget/doing_order.dart
+++ lib/widget/doing_order.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class DoingOrderList extends ConsumerWidget {
-  const DoingOrderList({
+  DoingOrderList({
     super.key,
   });
+
+  Future<void> _speak() async {
+    final FlutterTts flutterTts = FlutterTts();
+    String str = '测试自动播放语音';
+    // List<dynamic> languages = await flutterTts.getLanguages;
+    try {
+      await flutterTts.setLanguage("zh-CN"); // 设置语言为中文
+      await flutterTts.setPitch(1);
+      await flutterTts.setSpeechRate(0.8);
+      await flutterTts.speak(str);
+      print('speak success');
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -19,6 +35,10 @@ class DoingOrderList extends ConsumerWidget {
                 Text('Order 1'),
                 Text('Order 2'),
                 Text('Order 3'),
+                ElevatedButton(
+                  onPressed: _speak,
+                  child: Text('Speak Test'),
+                ),
               ],
             ),
           ),
